cb842a84d9e3caef6d4eaa9a7f457bf228eb8adb
BurakCaglar <burakcaglar@yandex.com>
2021-05-03 07:59:35 +0300

    context provider added

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { CityContextProvider } from "./context/context";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CityContextProvider>
+      <App />
+    </CityContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
