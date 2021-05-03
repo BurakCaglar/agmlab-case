c65c85a9b5ff8e038ae5b642146af3d58a50e63f
BurakCaglar <burakcaglar@yandex.com>
2021-05-03 08:01:46 +0300

    homepage added

---
+++ src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Map from "../components/Map/Map";
+import CityList from "../components/City/CityList";
+
+const HomePage = () => {
+  return (
+    <>
+      <CityList />
+      <Map />
+    </>
+  );
+};
+
+export default HomePage;
